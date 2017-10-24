7b3e3ef36f260f2a7d014568991cdb3a2dec80ab
Daniel Kinsman <danielkinsman@riseup.net>
2017-10-24 21:01:05 +1100

    add FlippableKinematicBody2D

--- FlippablePhysics2D/FlippablePhysics2D/FlippablePhysics2D.gd
+++ FlippablePhysics2D/FlippablePhysics2D/FlippablePhysics2D.gd
@@ -18,3 +18,22 @@ class FlippableRigidBody2D extends RigidBody2D:
 	
 	func set_flip_v(enabled):
 		helper.set_flip_v(enabled)
+
+class FlippableKinematicBody2D extends KinematicBody2D:
+	const FlipHelper = preload("FlipHelper.gd")
+	var helper = null
+	
+	func _ready():
+		helper = FlipHelper.new(self)
+	
+	func get_flip_h():
+		return helper.get_flip_h()
+	
+	func set_flip_h(enabled):
+		helper.set_flip_h(enabled)
+		
+	func get_flip_v():
+		return helper.get_flip_v()
+	
+	func set_flip_v(enabled):
+		helper.set_flip_v(enabled)
